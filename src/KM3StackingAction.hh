733e5ab50774780db38003729ddbf91f825f2f18
Moritz Lotze <lotzemoritz@gmail.com>
2016-03-09 15:59:04 +0100

    remove param from stackingaction header

--- src/KM3StackingAction.hh
+++ src/KM3StackingAction.hh
@@ -18,28 +18,9 @@ class KM3StackingAction : public G4UserStackingAction {
   virtual void NewStage();
   virtual void PrepareNewEvent();
   void SetDetector(KM3Detector *);
-#ifdef G4MYHAMUONS_PARAMETERIZATION
-  std::ofstream *outMuonHAFile;
-#endif
 
  private:
   KM3Detector *MyStDetector;
-#if !defined(G4MYEM_PARAMETERIZATION) && !defined(G4MYHA_PARAMETERIZATION)
-#ifdef G4ENABLE_MIE
-#ifndef G4DISABLE_PARAMETRIZATION
-  // delta rays parametrization section. Works only for muons primaries (not for
-  // showers ve)
-  std::vector<G4int> *idprikeep;
-  std::vector<G4double> *depenekeep;
-  std::vector<G4double> *timekeep;
-  std::vector<G4ThreeVector> *poskeep;
-  // before  G4int indexkeep;
-  KM3EMDeltaFlux *myFlux;
-  void CreateAllWaitingPhotons(void);
-////////////////////////////////////
-#endif
-#endif
-#endif
 
  protected:
 };
