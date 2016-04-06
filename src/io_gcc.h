6a7cd4800741a359c93136fcc58cc2733f269c19
Moritz Lotze <lotzemoritz@gmail.com>
2016-04-06 13:29:22 +0200

    reformat io_gcc

--- src/io_gcc.h
+++ src/io_gcc.h
@@ -7,25 +7,24 @@
 #include <sstream>
 #include <map>
 
-
-
 /**
   * multimap which holds the data of one event sorted
-  * according to the tags containg the data words a string 
+  * according to the tags containg the data words a string
   */
-typedef std::multimap<std::string, std::string, std::less<std::string> > ev_multimap;
+typedef std::multimap<std::string, std::string, std::less<std::string> >
+    ev_multimap;
 
 /**
   * map of iterators for the event-data multimap
-  * each iterator points to the current dataword for a specific tag 
+  * each iterator points to the current dataword for a specific tag
   */
-typedef std::map<std::string, ev_multimap::iterator, std::less<std::string> > ev_iter_map;
+typedef std::map<std::string, ev_multimap::iterator, std::less<std::string> >
+    ev_iter_map;
 
-/// an iterator for one particular tag 
+/// an iterator for one particular tag
 typedef ev_multimap::iterator ev_iter;
 
-
-/** 
+/**
   * handles I/O operation and modifiactions on a full event
   * which is stored as multimap of data-tags and data-words
   * <p>
@@ -34,48 +33,45 @@ typedef ev_multimap::iterator ev_iter;
   * @author J.Brunner
   * @version v1r4
   */
-class event
-
-//----------------------------------------------
-{
+class event {
  private:
-  /// multimap containing all data of one event 
+  /// multimap containing all data of one event
   ev_multimap evdata;
 
   /// map of iterators which point to one particular data word for each tag
   ev_iter_map evitmap;
 
   /// run identifier
-  unsigned ndrun;           
+  unsigned ndrun;
 
   /// event identifier
-  unsigned ndevt; 
+  unsigned ndevt;
 
   /// event type (like physics, slow control, detector, calibration)
-  unsigned ievtp; 
+  unsigned ievtp;
 
   /// start-of-run flag: true if a start_run event has been read
-  bool  new_run;  
+  bool new_run;
 
   /// event flag: true if a valid event has been read or defined
-  bool new_event; 
+  bool new_event;
 
-  /** 
+  /**
     * read header line from input stream <b>is</b>
     * @return error code
     * <ul>
     * <li> 0 - successfull operation
     * <li> 1 - read error on input stream
     * <li> 2 - EoF reached on input stream
-    * <li> 3 - wrong structure of input file (unexpected tag found) 
+    * <li> 3 - wrong structure of input file (unexpected tag found)
     * </ul>
     */
-  int readhead(std::istream& is); 
+  int readhead(std::istream& is);
 
   /**
     *  read event body from input stream <b>is</b>
     *  @return error code
-    *  <ul>      
+    *  <ul>
     *  <li> 0 - successfull operation
     *  <li> 1 - read error on input stream
     *  <li> 2 - EoF reached on input stream
@@ -90,7 +86,7 @@ class event
     * @param dt data tag (output)
     * @param dw data word (output)
     * @return error code
-    * <ul>      
+    * <ul>
     * <li> 0 - successfull operation
     * <li> 1 - read error on input stream
     * <li> 2 - EoF reached on input stream
@@ -99,11 +95,10 @@ class event
   int readline(std::istream& is, std::string& dt, std::string& dw);
 
  public:
-
   /**
     * default constructor
     */
-   event() : ndrun(0), ndevt(0), ievtp(0), new_run(false), new_event(false) {}
+  event() : ndrun(0), ndevt(0), ievtp(0), new_run(false), new_event(false) {}
 
   /**
     * clear current event from buffer
@@ -113,7 +108,7 @@ class event
   /**
     * read event from input stream <b>is</b>
     * @return error code
-    *  <ul>      
+    *  <ul>
     *  <li> 0 - successfull operation
     *  <li> 1 - read error on input stream
     *  <li> 2 - EoF reached on input stream
@@ -123,7 +118,7 @@ class event
   unsigned read(std::istream& is);
 
   /**
-    * define start-of-run event by specifying a new run-id 
+    * define start-of-run event by specifying a new run-id
     * @param nr run-identifier (input)
     * @return error code
     * <ul>
@@ -136,9 +131,9 @@ class event
   /**
     * define new event by specifying event-id and event type
     *  @param ne event-identifier
-    *  @param typ event type 
+    *  @param typ event type
     *  @return error code
-    *  <ul>      
+    *  <ul>
     *  <li> 0 - successful operation
     *  <li> 1 - no run defined
     *  <li> 2 - event-id not positive
@@ -149,31 +144,31 @@ class event
 
   /**
     * give number of data words for specified tag
-    * @param dt data tag 
+    * @param dt data tag
     * @return number of data words
     */
   int ndat(std::string dt);
 
   /**
     * give next data word for specified tag
-    * @param dt data tag 
+    * @param dt data tag
     * @return next data word
     */
   std::string next(std::string dt);
- 
+
   /**
-    * give event informations, all parameters are output 
+    * give event informations, all parameters are output
     * @param new_r true if current event is start-of-run
     * @param new_e true if an event is defined
     * @param nr run identifier
     * @param ne event identifier
     * @param typ event type
     */
-  void info(bool& new_r, bool& new_e, 
-      unsigned& nr, unsigned& ne, unsigned& typ);
+  void info(bool& new_r, bool& new_e, unsigned& nr, unsigned& ne,
+            unsigned& typ);
 
   /**
-    * @return true if start-of-run event has been read or defined, 
+    * @return true if start-of-run event has been read or defined,
     * otherwise false
     */
   bool run_header();
@@ -200,8 +195,8 @@ class event
 
   /**
     * write current event into output stream <b>os</b>
-    * @return 
-    * <ul> 
+    * @return
+    * <ul>
     * <li> 0 - successful operation
     * <li> 1 - no event defined
     * <li> 2 - write error on output stream
@@ -215,7 +210,7 @@ class event
     * @return number of data words which have been deleted
     */
   unsigned tagd(std::string dt);
-  
+
   /**
     * delete data words for specified tag at postion 'nligne'
     */
@@ -225,13 +220,13 @@ class event
     * add for specified tag new data word
     * @param dt data tag fro which a new word will be added
     * @param dw data word which will be added
-    * @return 
+    * @return
     * <ul>
     * <li> 0 - successful operation
     * <li> 1 - no event defined
     * </ul>
     */
-  unsigned taga(std::string dt,std::string dw);
+  unsigned taga(std::string dt, std::string dw);
 
   /**
     * read event after skipping events
@@ -259,11 +254,10 @@ class event
 
   /**
     * find run with specific id
-    * @param is input stream 
+    * @param is input stream
     * @param nr run-id to be found
     * @see event#read for return codes
     */
   unsigned findru(std::istream& is, unsigned nr);
-
 };
 #endif
