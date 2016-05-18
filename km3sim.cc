1080249a890cf631cb97cdaede33cc4f9450a062
Moritz Lotze <mlotze@km3net.de>
2016-05-18 14:12:57 +0200

    fix docopt typo

--- km3sim.cc
+++ km3sim.cc
@@ -1,9 +1,9 @@
+#include "docopt.h"
+
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-
-#include "docopt.h"
 
 #include "G4RunManager.hh"
 #include "G4UImanager.hh"
@@ -89,9 +89,10 @@ static const char USAGE[] =
     --no-mie          Disable mie scattering [default: false]
 )";
 
-int main(int argc, const char **argv) {
-  std::map<std::string, doctopt::value> args =
-      doctop::docopt(USAGE, {argv + 1, argv + argc}, true, "KM3Sim 2.0");
+int main(int argc, const char **argv)
+{
+  std::map<std::string, docopt::value> args =
+    doctop::docopt(USAGE, {argv + 1, argv + argc}, true, "KM3Sim 2.0");
 
   for (auto const &arg : args) {
     std::cout << arg.fist << arg.second << std::endl;
